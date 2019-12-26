412e2499b37bb650d592ca3cbe97f306cc2fe978
dodo-nainggolan <valdo.henschel@gmail.com>
2019-12-26 14:03:52 +0700

    Layout & view | 261220019

--- app/src/main/java/com/dicoding/picodiploma/academy/ui/home/HomeActivity.java
+++ app/src/main/java/com/dicoding/picodiploma/academy/ui/home/HomeActivity.java
@@ -35,7 +35,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
