59fb9a48003de222e4b1abd6bdc269684cca09aa
dodo-nainggolan <valdo.henschel@gmail.com>
2019-12-26 12:56:05 +0700

    Icon & Menu | 261220019

--- app/src/main/java/com/dicoding/picodiploma/academy/ui/home/HomeActivity.java
+++ app/src/main/java/com/dicoding/picodiploma/academy/ui/home/HomeActivity.java
@@ -19,14 +19,11 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.action_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.action_bookmark:
                     mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
